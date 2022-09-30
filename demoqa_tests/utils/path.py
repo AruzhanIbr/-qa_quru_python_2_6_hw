128c282637ef8493ac47191a1d25add1a28ff460
AruzhanIbr <ibragimova080499@gmail.com>
2022-10-01 04:37:09 +0600

    add path

--- demoqa_tests/utils/path.py
+++ demoqa_tests/utils/path.py
@@ -1,7 +1,8 @@
 from pathlib import Path
-
+from selene.support.shared import browser
 import resources
 
 
-def to_resource(relative_path):
-    return str(Path(resources.__file__).parent.joinpath(relative_path).absolute())
+def upload_picture(relative_path):
+    path = str(Path(resources.__file__).parent.joinpath(relative_path).absolute())
+    browser.element('#uploadPicture').send_keys(path)
